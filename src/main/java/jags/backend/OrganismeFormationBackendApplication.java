e5057a88b7e9fc5729fdc9b85043962e357838b5
Chebbi Siwar <siwarmohamed@live.fr>
2021-02-03 16:17:41 +0100

    first commit

---
+++ src/main/java/jags/backend/OrganismeFormationBackendApplication.java
@@ -0,0 +1,13 @@
+package jags.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganismeFormationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganismeFormationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jags/backend/OrganismeFormationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jags.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganismeFormationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
