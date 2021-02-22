b51760486c8159700e39d1827aab3e606612ff39
Chebbi Siwar <siwarmohamed@live.fr>
2021-02-22 21:47:40 +0100

    rajout des controllers services et repository pour les tables formations testPrerequis et responsable

---
+++ src/main/java/jags/backend/controllers/TestPrerequisController.java
@@ -0,0 +1,46 @@
+package jags.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jags.backend.entities.TestPrerequis;
+import jags.backend.services.TestPrerequisService;
+
+@RestController
+@RequestMapping("testPrerequis")
+@CrossOrigin
+
+public class TestPrerequisController {
+	@Autowired
+	private TestPrerequisService service;
+	
+	@GetMapping("numero/{numero}")
+	public List<TestPrerequis> findByNumero(@PathVariable String numero) {
+		return service.findByNumero(numero);
+	}
+	
+	@GetMapping
+	public List<TestPrerequis> findAll() {
+		return service.findAll();
+	}
+	
+	@GetMapping("{id}")
+	public TestPrerequis findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+	
+	@PostMapping
+	public <S extends TestPrerequis> S save(@RequestBody S entity) {
+		return service.save(entity);
+	}
+	
+
+}
---
+++ src/main/java/jags/backend/repositories/ResponsableRepository.java
@@ -0,0 +1,15 @@
+package jags.backend.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jags.backend.entities.Responsable;
+
+
+public interface ResponsableRepository extends JpaRepository<Responsable, Long>{
+	public List<Responsable> findByNom(String nom);
+	public List<Responsable> findByPrenom(String prenom);
+	public List<Responsable> findByEmail(String email);
+	
+}
